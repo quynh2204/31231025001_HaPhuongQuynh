7927e55292877c5c2ec5803a8754897808f48468
quynhha2204 <phuongquynh0204cm@gmail.com>
2025-03-28 04:25:04 +0700

    done 9

---
+++ Module1BaiSo9_HaPhuongQuynh/LinkLabelDemo.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Module1BaiSo9_HaPhuongQuynh
+{
+    partial class LinkLabelDemo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(153, 73);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(251, 20);
+            linkLabel1.TabIndex = 0;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Windows Forms Community Website";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(164, 181);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(229, 20);
+            linkLabel2.TabIndex = 1;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Launch Calculator | Open C: Drive";
+            linkLabel2.LinkClicked += this.linkLabel2_LinkClicked;
+            // 
+            // LinkLabelDemo
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(607, 270);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Name = "LinkLabelDemo";
+            Text = "LinkLabelDemo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+    }
+}
---
+++ Module1BaiSo9_HaPhuongQuynh/LinkLabelDemo.cs
@@ -0,0 +1,42 @@
+﻿namespace Module1BaiSo9_HaPhuongQuynh
+{
+    public partial class LinkLabelDemo : Form
+    {
+        public LinkLabelDemo()
+        {
+            InitializeComponent();
+
+            // Gán dữ liệu cho từng phần của linkLabel2
+            linkLabel2.Links.Add(0, "Launch Calculator".Length, "calc.exe");
+            linkLabel2.Links.Add(linkLabel2.Text.IndexOf("Open C: Drive"), "Open C: Drive".Length, "C:\\");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                var url = "https://learn.microsoft.com/en-us/visualstudio/ide/create-csharp-winform-visual-studio?view=vs-2022";
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi mở trang web: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (e.Link.LinkData.ToString() == "calc.exe")
+            {
+                System.Diagnostics.Process.Start("calc.exe"); // Mở Calculator
+            }
+            else if (e.Link.LinkData.ToString() == "C:\\")
+            {
+                System.Diagnostics.Process.Start("explorer.exe", "C:\\"); // Mở ổ C
+            }
+        }
+    }
+}
