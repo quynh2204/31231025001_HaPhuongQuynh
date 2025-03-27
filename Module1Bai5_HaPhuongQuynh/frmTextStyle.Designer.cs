10e0b69304f14cade9238e73b6dec22aa71b1780
quynhha2204 <phuongquynh0204cm@gmail.com>
2025-03-28 01:43:54 +0700

    done 5

--- Module1Bai5_HaPhuongQuynh/frmTextStyle.Designer.cs
+++ Module1Bai5_HaPhuongQuynh/frmTextStyle.Designer.cs
@@ -29,18 +29,19 @@
         private void InitializeComponent()
         {
             txtNhapTen = new TextBox();
-            lblLapTrinh = new Label();
-            radioButton1 = new RadioButton();
-            radioButton2 = new RadioButton();
-            radioButton3 = new RadioButton();
-            checkBox1 = new CheckBox();
-            checkBox2 = new CheckBox();
+            text = new Label();
+            radRed = new RadioButton();
+            radGreen = new RadioButton();
+            radBlue = new RadioButton();
+            chkItalic = new CheckBox();
             groupBox1 = new GroupBox();
+            radBlack = new RadioButton();
             groupBox2 = new GroupBox();
-            radioButton4 = new RadioButton();
-            checkBox3 = new CheckBox();
+            chkBold = new CheckBox();
+            chkUnderline = new CheckBox();
             button1 = new Button();
             label1 = new Label();
+            lblLapTrinh = new Label();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             SuspendLayout();
@@ -51,115 +52,133 @@
             txtNhapTen.Name = "txtNhapTen";
             txtNhapTen.Size = new Size(125, 27);
             txtNhapTen.TabIndex = 0;
+            txtNhapTen.TextChanged += txtNhapTen_TextChanged;
             // 
-            // lblLapTrinh
+            // text
             // 
-            lblLapTrinh.AutoSize = true;
-            lblLapTrinh.Location = new Point(210, 357);
-            lblLapTrinh.Name = "lblLapTrinh";
-            lblLapTrinh.Size = new Size(50, 20);
-            lblLapTrinh.TabIndex = 1;
-            lblLapTrinh.Text = "label1";
+            text.AutoSize = true;
+            text.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            text.ForeColor = Color.IndianRed;
+            text.Location = new Point(85, 359);
+            text.Name = "text";
+            text.Size = new Size(101, 20);
+            text.TabIndex = 1;
+            text.Text = "Lập Trình Bởi";
             // 
-            // radioButton1
+            // radRed
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(32, 47);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(117, 24);
-            radioButton1.TabIndex = 2;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "radioButton1";
-            radioButton1.UseVisualStyleBackColor = true;
+            radRed.AutoSize = true;
+            radRed.ForeColor = Color.Red;
+            radRed.Location = new Point(32, 47);
+            radRed.Name = "radRed";
+            radRed.Size = new Size(56, 24);
+            radRed.TabIndex = 2;
+            radRed.TabStop = true;
+            radRed.Text = "Red";
+            radRed.UseVisualStyleBackColor = true;
+            radRed.CheckedChanged += radColor_CheckedChanged;
             // 
-            // radioButton2
+            // radGreen
             // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(32, 97);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(117, 24);
-            radioButton2.TabIndex = 3;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "radioButton2";
-            radioButton2.UseVisualStyleBackColor = true;
+            radGreen.AutoSize = true;
+            radGreen.ForeColor = Color.Green;
+            radGreen.Location = new Point(32, 97);
+            radGreen.Name = "radGreen";
+            radGreen.Size = new Size(69, 24);
+            radGreen.TabIndex = 3;
+            radGreen.TabStop = true;
+            radGreen.Text = "Green";
+            radGreen.UseVisualStyleBackColor = true;
+            radGreen.CheckedChanged += radColor_CheckedChanged;
             // 
-            // radioButton3
+            // radBlue
             // 
-            radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(32, 155);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(117, 24);
-            radioButton3.TabIndex = 4;
-            radioButton3.TabStop = true;
-            radioButton3.Text = "radioButton3";
-            radioButton3.UseVisualStyleBackColor = true;
+            radBlue.AutoSize = true;
+            radBlue.ForeColor = Color.Blue;
+            radBlue.Location = new Point(32, 155);
+            radBlue.Name = "radBlue";
+            radBlue.Size = new Size(59, 24);
+            radBlue.TabIndex = 4;
+            radBlue.TabStop = true;
+            radBlue.Text = "Blue";
+            radBlue.UseVisualStyleBackColor = true;
+            radBlue.CheckedChanged += radColor_CheckedChanged;
             // 
-            // checkBox1
+            // chkItalic
             // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(62, 48);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(101, 24);
-            checkBox1.TabIndex = 5;
-            checkBox1.Text = "checkBox1";
-            checkBox1.UseVisualStyleBackColor = true;
-//            checkBox1.CheckedChanged += this.checkBox1_CheckedChanged;
-            // 
-            // checkBox2
-            // 
-            checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(62, 117);
-            checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(101, 24);
-            checkBox2.TabIndex = 6;
-            checkBox2.Text = "checkBox2";
-            checkBox2.UseVisualStyleBackColor = true;
+            chkItalic.AutoSize = true;
+            chkItalic.Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            chkItalic.Location = new Point(62, 117);
+            chkItalic.Name = "chkItalic";
+            chkItalic.Size = new Size(113, 24);
+            chkItalic.TabIndex = 6;
+            chkItalic.Text = "Nghiên Italic";
+            chkItalic.UseVisualStyleBackColor = true;
+            chkItalic.CheckedChanged += chkItalic_CheckedChanged;
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(radioButton4);
-            groupBox1.Controls.Add(radioButton1);
-            groupBox1.Controls.Add(radioButton2);
-            groupBox1.Controls.Add(radioButton3);
+            groupBox1.BackColor = SystemColors.ActiveCaption;
+            groupBox1.Controls.Add(radBlack);
+            groupBox1.Controls.Add(radRed);
+            groupBox1.Controls.Add(radGreen);
+            groupBox1.Controls.Add(radBlue);
             groupBox1.Location = new Point(53, 78);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(230, 256);
             groupBox1.TabIndex = 7;
             groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
+            groupBox1.Text = "Color";
+            // 
+            // radBlack
+            // 
+            radBlack.AutoSize = true;
+            radBlack.ForeColor = Color.Black;
+            radBlack.Location = new Point(32, 203);
+            radBlack.Name = "radBlack";
+            radBlack.Size = new Size(65, 24);
+            radBlack.TabIndex = 5;
+            radBlack.TabStop = true;
+            radBlack.Text = "Black";
+            radBlack.UseVisualStyleBackColor = true;
+            radBlack.CheckedChanged += radColor_CheckedChanged;
             // 
             // groupBox2
             // 
-            groupBox2.Controls.Add(checkBox3);
-            groupBox2.Controls.Add(checkBox2);
-            groupBox2.Controls.Add(checkBox1);
+            groupBox2.BackColor = Color.LightSalmon;
+            groupBox2.Controls.Add(chkBold);
+            groupBox2.Controls.Add(chkUnderline);
+            groupBox2.Controls.Add(chkItalic);
             groupBox2.Location = new Point(395, 78);
             groupBox2.Name = "groupBox2";
             groupBox2.Size = new Size(250, 256);
             groupBox2.TabIndex = 8;
             groupBox2.TabStop = false;
-            groupBox2.Text = "groupBox2";
+            groupBox2.Text = "Font";
             // 
-            // radioButton4
+            // chkBold
             // 
-            radioButton4.AutoSize = true;
-            radioButton4.Location = new Point(32, 203);
-            radioButton4.Name = "radioButton4";
-            radioButton4.Size = new Size(117, 24);
-            radioButton4.TabIndex = 5;
-            radioButton4.TabStop = true;
-            radioButton4.Text = "radioButton4";
-            radioButton4.UseVisualStyleBackColor = true;
+            chkBold.AutoSize = true;
+            chkBold.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            chkBold.Location = new Point(62, 55);
+            chkBold.Name = "chkBold";
+            chkBold.Size = new Size(100, 24);
+            chkBold.TabIndex = 8;
+            chkBold.Text = "Đậm Bold";
+            chkBold.UseVisualStyleBackColor = true;
+            chkBold.CheckedChanged += chkBold_CheckedChanged;
             // 
-            // checkBox3
+            // chkUnderline
             // 
-            checkBox3.AutoSize = true;
-            checkBox3.Location = new Point(62, 182);
-            checkBox3.Name = "checkBox3";
-            checkBox3.Size = new Size(101, 24);
-            checkBox3.TabIndex = 7;
-            checkBox3.Text = "checkBox3";
-            checkBox3.UseVisualStyleBackColor = true;
+            chkUnderline.AutoSize = true;
+            chkUnderline.Font = new Font("Segoe UI", 9F, FontStyle.Underline, GraphicsUnit.Point, 0);
+            chkUnderline.Location = new Point(62, 182);
+            chkUnderline.Name = "chkUnderline";
+            chkUnderline.Size = new Size(101, 24);
+            chkUnderline.TabIndex = 7;
+            chkUnderline.Text = "Gạch Chân";
+            chkUnderline.UseVisualStyleBackColor = true;
+            chkUnderline.CheckedChanged += chkUnderline_CheckedChanged;
             // 
             // button1
             // 
@@ -169,29 +188,44 @@
             button1.TabIndex = 9;
             button1.Text = "Thoát";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += btnThoat_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.ForeColor = Color.LawnGreen;
             label1.Location = new Point(106, 32);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(72, 20);
             label1.TabIndex = 10;
-            label1.Text = "label1";
+            label1.Text = "Nhập Tên";
+            // 
+            // lblLapTrinh
+            // 
+            lblLapTrinh.AutoSize = true;
+            lblLapTrinh.Location = new Point(210, 359);
+            lblLapTrinh.Name = "lblLapTrinh";
+            lblLapTrinh.Size = new Size(50, 20);
+            lblLapTrinh.TabIndex = 11;
+            lblLapTrinh.Text = "label2";
             // 
             // frmTextStyle
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblLapTrinh);
             Controls.Add(label1);
             Controls.Add(button1);
-            Controls.Add(lblLapTrinh);
+            Controls.Add(text);
             Controls.Add(txtNhapTen);
             Controls.Add(groupBox1);
             Controls.Add(groupBox2);
             Name = "frmTextStyle";
-            Text = "Form1";
+            Text = "Định dạng (Fomater)";
+            Load += frmTextStyle_Load;
+            KeyDown += frmTextStyle_KeyDown;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
@@ -203,17 +237,19 @@
         #endregion
 
         private TextBox txtNhapTen;
-        private Label lblLapTrinh;
-        private RadioButton radioButton1;
-        private RadioButton radioButton2;
-        private RadioButton radioButton3;
-        private CheckBox checkBox1;
-        private CheckBox checkBox2;
+        private Label text;
+        private RadioButton radGreen;
+        private RadioButton radBlue;
+
+        private CheckBox chkItalic;
         private GroupBox groupBox1;
         private GroupBox groupBox2;
-        private RadioButton radioButton4;
-        private CheckBox checkBox3;
+        private RadioButton radBlack;
+        private CheckBox chkUnderline;
         private Button button1;
         private Label label1;
+        private CheckBox chkBold;
+        private Label lblLapTrinh;
+        private RadioButton radRed;
     }
 }
--- Module1Bai5_HaPhuongQuynh/frmTextStyle.cs
+++ Module1Bai5_HaPhuongQuynh/frmTextStyle.cs
@@ -1,10 +1,86 @@
-namespace Module1Bai5_HaPhuongQuynh
+﻿namespace Module1Bai5_HaPhuongQuynh
 {
     public partial class frmTextStyle : Form
     {
         public frmTextStyle()
         {
             InitializeComponent();
+        }
+
+        private void frmTextStyle_Load(object sender, EventArgs e)
+        {
+            radRed.Checked = true; // Chọn mặc định
+            lblLapTrinh.ForeColor = Color.Red; // Đặt màu chữ mặc định là đỏ
+            txtNhapTen.ForeColor = Color.Red; // Màu nhập liệu cũng là đỏ
+            txtNhapTen.Focus(); // Đặt con trỏ vào ô nhập tên
+        }
+
+        private void txtNhapTen_TextChanged(object sender, EventArgs e)
+        {
+            lblLapTrinh.Text = txtNhapTen.Text;
+        }
+        private void radColor_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton r = (RadioButton)sender;
+
+            switch (r.Name)
+            {
+                case "radRed":
+                    lblLapTrinh.ForeColor = Color.Red;
+                    txtNhapTen.ForeColor = Color.Red;
+                    break;
+                case "radGreen":
+                    lblLapTrinh.ForeColor = Color.Green;
+                    txtNhapTen.ForeColor = Color.Green;
+                    break;
+                case "radBlue":
+                    lblLapTrinh.ForeColor = Color.Blue;
+                    txtNhapTen.ForeColor = Color.Blue;
+                    break;
+                case "radBlack":
+                    lblLapTrinh.ForeColor = Color.Black;
+                    txtNhapTen.ForeColor = Color.Black;
+                    break;
+            }
+        }
+        private void UpdateFontStyle()
+        {
+            FontStyle style = FontStyle.Regular;
+
+            if (chkBold.Checked)
+                style |= FontStyle.Bold;
+            if (chkItalic.Checked)
+                style |= FontStyle.Italic;
+            if (chkUnderline.Checked)
+                style |= FontStyle.Underline;
+
+            lblLapTrinh.Font = new Font(lblLapTrinh.Font.FontFamily, lblLapTrinh.Font.Size, style);
+            txtNhapTen.Font = new Font(txtNhapTen.Font.FontFamily, txtNhapTen.Font.Size, style);
+        }
+        private void chkBold_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateFontStyle();
+        }
+
+        private void chkItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateFontStyle();
+        }
+
+        private void chkUnderline_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateFontStyle();
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void frmTextStyle_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
         }
     }
 }
